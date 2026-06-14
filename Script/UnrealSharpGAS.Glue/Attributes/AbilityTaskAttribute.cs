835df6a8900f923a0f1117d9ab24e0a7ea83fc37
ErrorWings <error_wings@qq.com>
2026-06-15 02:30:56 +0800

    Revert: We cannot achieve this using source generators
    This reverts commit 725159254ca7a61092a30492aeaa3ab7bf7bc16f.

--- Script/UnrealSharpGAS.Glue/Attributes/AbilityTaskAttribute.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace UnrealSharp.UnrealSharpGAS;
-
-[AttributeUsage(AttributeTargets.Method)]
-public sealed class AbilityTaskAttribute(string category = "Ability|Tasks") : Attribute
-{
-	public string Category => category;
-}
